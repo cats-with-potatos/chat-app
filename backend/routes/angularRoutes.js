6a8287064963ab5038c9c3685155027db7b6affb
RafGDev <rafaelgoesmann@gmail.com>
2016-08-24 18:05:53 +1200

    Fixed issue with index.html path

--- backend/routes/angularRoutes.js
+++ backend/routes/angularRoutes.js
@@ -1,12 +1,14 @@
-const angularRoutes = {};
+const path = require("path")
+, angularRoutes = {};
 
 angularRoutes.serveIndex = (req, res, next) => {
   //[JG]: Anguar routes are configured to not be hash prefixed.
+
   if (req.url.indexOf('/api') === 0) {
     next();
   }
   else {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
   }
 };
 
