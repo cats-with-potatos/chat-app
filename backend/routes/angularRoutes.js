eed6eebadde4a62324158aef7f9cfa2d32a89e6a
RafGDev <rafaelgoesmann@gmail.com>
2016-08-28 16:10:45 +1200

    deleted .indexOf check in serveIndex route

--- backend/routes/angularRoutes.js
+++ backend/routes/angularRoutes.js
@@ -3,13 +3,7 @@ const path = require("path")
 
 angularRoutes.serveIndex = (req, res, next) => { // TYPE: GET
   //[JG]: Anguar routes are configured to not be hash prefixed.
-
-  if (req.url.indexOf('/api') === 0) {
-    next();
-  }
-  else {
     res.sendFile(path.join(__dirname, "../public", "index.html"));
-  }
 };
 
 module.exports = angularRoutes;
