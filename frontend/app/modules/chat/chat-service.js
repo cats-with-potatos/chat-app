69c7e2850390981cdff433dea98846fe01533429
Joe Daoud <joedaoud@outlook.com>
2016-09-21 21:10:13 -0700

    Revert "Create signinFunctionality Branch"
    This reverts commit e7db15bb8536da0255952f6647bdb48b13628f9c.

---
+++ frontend/app/modules/chat/chat-service.js
@@ -0,0 +1,68 @@
+(function() {
+  angular
+  .module('chat-app.chat')
+  .service('ChatService', ['$http', Service]);
+
+  function Service($http) {
+    var service = this;
+
+    service.getChatMessages = function(channel) {
+      return $http(
+        {
+          method: "GET",
+          url: "/api/getChannelMessages?channelId=" + channel.channelId,
+          headers: {
+            Authorization: "Bearer " + Cookies.get("auth"),
+          }
+        })
+        .then(function(res) {
+          const messagesArray = [];
+
+          res.data.data.forEach(function(val) {
+            val.contents = JSON.parse(val.contents); //Parses the stringified string
+            messagesArray.push(val);
+          });
+
+          return messagesArray; // Returns data in an array
+        })
+      };
+
+      //Sends a message to the server.
+      service.sendMessage = function(data) {
+        return $http({
+          method: "POST",
+          url: "/api/sendChatMessage",
+          data: $.param({channelId: data.channelId, message: data.message}),
+          headers: {
+            Authorization: "Bearer " + Cookies.get("auth"),
+            'Content-Type': "application/x-www-form-urlencoded",
+          }
+        })
+      };
+
+      //Sends to the server that the user is typing
+      service.sendUserIsTyping = function() {
+        return $http({
+          method: "POST",
+          url: "/api/sendUserIsTyping",
+          headers: {
+            Authorization: "Bearer " + Cookies.get("auth"),
+            'Content-Type': "application/x-www-form-urlencoded",
+          }
+        });
+      };
+
+      //Sends to the server that the user has stopped typing
+      service.sendUserStoppedTyping = function() {
+        return $http({
+          method: "POST",
+          url: "/api/sendUserStoppedTyping",
+          headers: {
+            Authorization: "Bearer " + Cookies.get("auth"),
+            'Content-Type': "application/x-www-form-urlencoded",
+          }
+        });
+      };
+      return service;
+    }
+  })();
